80c4b0d9c5a885da31da99975e069ec3e44cf676
kim-byoungkwan <52727682+kim-byoungkwan@users.noreply.github.com>
2021-08-14 23:54:46 +0900

    Add files via upload

--- 백준_그리디_2212_ebiz_G5.py
+++ 백준_그리디_2212_ebiz_G5.py
@@ -1,5 +1,27 @@
-N = int(input())
-K = int(input())
+import sys
 
-position = list(map(int,input().split()))
+N = int(sys.stdin.readline())
+K = int(sys.stdin.readline())
+
+position = sorted(list(map(int,sys.stdin.readline().split())))
+
+box = []
+
+if K >= N:
+
+    print(0)
+
+else:
+
+    for i in range(1,len(position)):
+
+        box.append(position[i] - position[i-1])
+
+    box.sort(reverse=True)
+
+    for i in range(K-1):
+
+        box.pop(0)
+
+    print(sum(box))
 
